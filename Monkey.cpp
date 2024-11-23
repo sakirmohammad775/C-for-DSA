37159f1940c494aee5164c0d8f10df9afdd23de1
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-23 07:33:33 +0600

    add typecasting

---
+++ Monkey.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool
+int main(){
+    str s;
+
+
+}
---
+++ Sort_it_2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sort_it(){
+}
+int main(){
+    return 0;
+}
