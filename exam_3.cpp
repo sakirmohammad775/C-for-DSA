755170534c21fff7b0d553e6ce42f798581187b9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-23 12:56:34 +0600

    add simple calculator

--- exam_3.cpp
+++ exam_3.cpp
@@ -1,9 +1,25 @@
-#include<iostream> 
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
 using namespace std;
-int main(){
-    int x;
-    char c='a';
-    cin>>x;
-    cout<<x << (char)x<<endl;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n / 2; i++)
+    {
+        swap(a[i], a[n - i - 1]);
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
     return 0;
 }
