55ef0224b937fbafc1b80e6ed61d4a191d2edea2
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-24 23:53:32 +0600

    add amrks and sort

--- Dynamic_resulation.cpp
+++ Dynamic_resulation.cpp
@@ -3,16 +3,25 @@ using namespace std;
 int main()
 {
     char s[10000];
-    cin.getline(s, 10000);
+    while (cin.getline(s, 10000))
+    {
+        char exchange[10000];
+        int index=0;
 
-    int n = strlen(s);
-    sort(s, s + n);
-    for(int i = 0; i < n; i++){
-        if(s[i]!=' '){
-            cout<<s[i];
+        int n = strlen(s);
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] != ' ')
+            {
+                    exchange[index++] = s[i];
+            }
         }
+        sort(exchange,exchange+index);
+        for(int i=0; i<index; i++){
+            cout << exchange[i];
+        }
+        cout << endl;
     }
-
 
     return 0;
 }
