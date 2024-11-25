7682feb7a3050b2de3e9a20edbb40248dd6109ec
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-25 23:10:22 +0600

    string capacity function

--- string.cpp
+++ string.cpp
@@ -1,13 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char s1[10]="hello";
-    char s2[10]="helloo";
-    if(strcmp(s1,s2)==0){
+    string s1="hello";
+    s1="hello world in bd";
+    string s2="hello";
+    if(s1==s2){
         cout<<"Equal strings"<<endl;
     }
     else{
         cout<<"Unequal strings"<<endl;
+        cout<<s1<<endl;
     }
     return 0;
 }
