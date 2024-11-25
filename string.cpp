48cd593abfccc937b6c2532144489d1df1982637
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-25 22:51:42 +0600

    strcmp string

--- string.cpp
+++ string.cpp
@@ -1,7 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    char s[10]="hello";
-    cout<<s<<endl;
+    char s1[10]="hello";
+    char s2[10]="helloo";
+    if(strcmp(s1,s2)==0){
+        cout<<"Equal strings"<<endl;
+    }
+    else{
+        cout<<"Unequal strings"<<endl;
+    }
     return 0;
 }
