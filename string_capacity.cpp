d21040177002d86b6163b0d41d3501e7ce60f6b8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-26 22:11:35 +0600

    add string capacity

--- string_capacity.cpp
+++ string_capacity.cpp
@@ -1,7 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string s="hello";
-   cout<< s.size(); 
+int main()
+{
+    string s = "hello";
+    
+    cout<<s.capacity()<<endl;
     return 0;
 }
