4148df8de521849a5db689696fb9f437c1101118
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-28 23:52:23 +0600

    add element access

--- Element_Access.cpp
+++ Element_Access.cpp
@@ -4,9 +4,10 @@ int main(){
     string s;
     cin >> s;
     cout << s[0] << endl;
-    cout << s.at(1) << endl;
-    cout<<s.front() << endl;
-    cout<<s.back() << endl;
+    string s2="Hi ";
+    s=s2;
+    s+=s2;
+    cout<<s<<endl;
 
     return 0;
 }
