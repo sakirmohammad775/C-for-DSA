2f7002cc322d93875d93e4bb8a319f94dd100a43
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-29 15:48:08 +0600

    function inside class

---
+++ function_inside_class.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+public:
+string name;
+int roll;
+int age;
+};
+int main(){
+    return 0;
+}
---
+++ range_based_for.cpp
@@ -0,0 +1,19 @@
+//reverse word codeforces solution
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+    ss>>word;
+    reverse(word.begin(), word.end());
+    cout << word ;
+    while (ss >> word)
+    {
+        reverse(word.begin(), word.end());
+        cout<< " "<< word ;
+    }
+    return 0;
+}
