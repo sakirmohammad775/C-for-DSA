ca3da994e9d0180490204289967b2442d5ec3bf2
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-30 14:10:47 +0600

    add function in class

---
+++ Function_in_class.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    string name;
+    int roll;
+    int age;
+    Student(string name, int roll)
+    {
+        this->name = name;
+        this->roll = roll;
+    }
+};
+
+int main()
+{
+    Student student1("John Doe", 123);
+    return 0;
+}
