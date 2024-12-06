cd50cf80066551b3042e9953303ea21d1742cd52
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-06 21:53:10 +0600

    add new features

--- Reverse_word.cpp
+++ Reverse_word.cpp
@@ -1,43 +1,51 @@
-/**
- Problem Statement
-
-You will be given two strings S and X. You need to replace all X from string S with a '#' sign.
-
-Input Format
-
-First line will contain T, the number of test cases.
-Next T lines will contain a line with S and X.
-Constraints
-
-1 <= T <= 1000
-1 <= |S|, |X| <= 1000
-|X| <= |S|
-Output Format
-
-For each test cases output the modified string S.
-Sample Input 0
-
-2
-
- */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     int t;
-    cin>>t;
-    
-    while(t--){
-        string s,x;
-        cin>>s>>x;
-        for(int i=0;i<s.size();i++){
-            if(s.substr(i,x.size())==x)
-                s.replace(i,x.size(),"#"s.substr(i,x.size()));
+    cin >> t; 
+    cin.ignore(); 
 
+    while (t--) {
+        string word;
+        getline(cin, word);
+        string s = "", x = "";
+        bool space = false;
+        for (char c : word) {
+            if (c == ' ' && !space) {
+                space = true;
+            } 
+            else if (!space) {
+                s += c; 
+            } 
+            else {
+                x += c; 
+            }
         }
+        string reverseWord = "";  
+        int i = 0;
+        int lengthOfS = s.length();
+        int lengthOfX = x.length();
 
-        cout<<s<<endl;
+        while (i < lengthOfS) {
+            bool match = true;
 
+            for (int j = 0; j < lengthOfX; j++) {
+                if (i + j >= lengthOfS || s[i + j] != x[j]) {
+                    match = false;
+                    break;
+                }
+            }
 
+            if (match) {
+                reverseWord += "#";
+                i += lengthOfX; 
+            } else {
+                reverseWord += s[i];
+                i++;  
+            }
+        }
+        cout << reverseWord << endl;
     }
 
     return 0;
