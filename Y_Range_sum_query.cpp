45eda150bfdfbc213781f6fc76accaa28510aef5
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-15 11:01:08 +0600

    add range sum query

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> v(n + 1);
+    for(int i = 1; i <= n; i++){//o(N)
+        cin >> v[i];
+    }
+    while(q--){//o(q)
+         int l,r;
+         cin >> l >> r;
+         int sum=0;
+         for(int i = l; i <= r; i++){//o(n)
+            sum += v[i];
+         }//o(QN)
+         cout << sum << endl;
+    }
+    return 0;
+}
+//final complexity o(qn)
