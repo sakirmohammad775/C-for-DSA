49c2fd70fa9a44a2977e646ac5a2d3d186074194
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-18 17:34:59 +0600

    add to the server

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -8,14 +8,24 @@ int main()
     for(int i = 1; i <= n; i++){//o(N)
         cin >> v[i];
     }
+    vector<int> pre(n + 1);
+    pre[1] =v[0];
+    for(int i = 1; i <= n; i++){
+        pre[i]=pre[i-1]+v[i];
+    }
+
     while(q--){//o(q)
          int l,r;
          cin >> l >> r;
-         int sum=0;
+         int sum;
          //time limit exceed !! so reduce... 
-         for(int i = l; i <= r; i++){//o(n)
-            sum += v[i];
-         }//o(QN)
+         if(l==1){
+            sum=pre[r];
+         }
+         else{
+
+         int sum=pre[r]-pre[l-1];
+         }
          cout << sum << endl;
     }
     return 0;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,8 @@
-cout<<(b?"YES":"NO")<<endl;
+while(q--){//o(q)
+    //      int l,r;
+    //      cin >> l >> r;
+    //      int sum=0;
+    //      //time limit exceed !! so reduce... 
+    //      int sum=pre[r]-pre[l];
+    //      cout << sum << endl;
+    // }
