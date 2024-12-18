29d499e1c65e2965ee2d3c9a50ed91c6b5e6f0d7
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-18 19:11:31 +0600

    add to the server

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -5,29 +5,34 @@ int main()
     int n, q;
     cin >> n >> q;
     vector<int> v(n + 1);
-    for(int i = 1; i <= n; i++){//o(N)
+    for (int i = 1; i <= n; i++)
+    { // o(N)
         cin >> v[i];
     }
     vector<int> pre(n + 1);
-    pre[1] =v[0];
-    for(int i = 1; i <= n; i++){
-        pre[i]=pre[i-1]+v[i];
+    pre[1] = v[1];
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i - 1] + v[i];
     }
 
-    while(q--){//o(q)
-         int l,r;
-         cin >> l >> r;
-         int sum;
-         //time limit exceed !! so reduce... 
-         if(l==1){
-            sum=pre[r];
-         }
-         else{
+    while (q--)
+    { // o(q)
+        int l, r;
+        cin >> l >> r;
+        int sum;
+        // time limit exceed !! so reduce...
+        if (l == 1)
+        {
+            sum = pre[r];
+        }
+        else
+        {
 
-         int sum=pre[r]-pre[l-1];
-         }
-         cout << sum << endl;
+            sum = pre[r] - pre[l - 1];
+        }
+        cout << sum << endl;
     }
     return 0;
 }
-//final complexity o(qn)
+// final complexity o(qn)
