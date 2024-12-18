1087439201504bd2865cbaf691783940de9fb329
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-18 21:02:51 +0600

    add to the server

--- Binery_Search.cpp
+++ Binery_Search.cpp
@@ -12,11 +12,23 @@ int main()
     int value;
     cin >> value;
     int flag = 0;
-    for (int i = 0; i < n; i++)
+    int l = 0;
+    int r = n - 1;
+    while (r>l)
     {
-        if (a[i] == value)
+        int mid = (l + r) / 2;
+        if (a[mid] == value)
         {
             flag = 1;
+            break;
+        }
+        else if (a[mid] > value)
+        {
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
         }
     }
     if (flag == 1)
--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -4,22 +4,35 @@ int main()
 {
     int n, q;
     cin >> n >> q;
-    vector<int> a(n);
+    int a[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    
-    while (q--)
+    sort(a, a + n);
+    for (int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
         int flag = 0;
-        for (int i = 0; i < n; i++)
+        int l = 0;
+        int r = l - 1;
+
+        while (l<=r)
         {
-            if (a[i] == x)
+            int mid = (l + r) / 2;
+            if (a[mid] == x)
             {
                 flag = 1;
+                break;
+            }
+            else if (a[mid] < x)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
             }
         }
         if (flag == 1)
@@ -30,7 +43,7 @@ int main()
         {
             cout << "not found" << endl;
         }
-
     }
-        return 0;
+
+    return 0;
 }
