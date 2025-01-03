8003b34a95c36684ac4746784e7eb3c4f8552538
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-01-03 21:04:25 +0600

    add new node

--- delete_at_any_pos_Doubly.cpp
+++ delete_at_any_pos_Doubly.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Node {
+class Node
+{
 public:
     int val;
     Node *next;
-    Node* prev;
-    Node(int val) {
+    Node *prev;
+    Node(int val)
+    {
         this->val = val;   // Initialize the node's value with the given 'val'
         this->next = NULL; // Initialize next pointer to NULL
-        this->prev=NULL; // Initialize prev pointer to NULL
-    }f
+        this->prev = NULL; // Initialize prev pointer to NULL
+    }
 };
 void print_insert(Node *&head)
 {
@@ -21,20 +23,21 @@ void print_insert(Node *&head)
     }
     cout << endl;
 }
-void delete_at_any_pos(Node* &head,int idx){
-    Node* tmp=head;
-    for(int i=1;i<idx;i++){
-        tmp=tmp->next;
+void delete_at_any_pos(Node *&head, int idx)
+{
+    Node *tmp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        tmp = tmp->next;
     }
-    Node* deletenode=tmp->next;
-    tmp->next=tmp->next->next;
-    tmp->next->prev=tmp;
+    Node *deletenode = tmp->next;
+    tmp->next = tmp->next->next;
+    tmp->next->prev = tmp;
     delete deletenode;
-
-    
 }
 
-int main() {
+int main()
+{
     Node *head = new Node(10);
     Node *a = new Node(20);
     Node *tail = new Node(30);
@@ -44,7 +47,7 @@ int main() {
 
     a->next = tail;
     tail->prev = a;
-    delete_at_any_pos(head,1);
+    delete_at_any_pos(head, 1);
     print_insert(head);
     return 0;
 }
